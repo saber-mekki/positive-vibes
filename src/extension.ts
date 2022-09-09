09d09865838e6f8ef4f8ec4c4bc7a07b1a4017b8
Saber <saber.mekki@aureskonnect.com>
2022-09-09 15:42:18 +0100

    feat: add `postives vibe data` array in `extention` file

--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-var cron = require('node-cron');
+import * as vscode from "vscode";
+var cron = require("node-cron");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const postivesVibeData = [
+  "No bad vibes",
+  "Let it challenge you to change you!",
+  "Let your thoughts go. Why focus on something you can’t control?",
+  "Do life with love!",
+  "Stop when you’re happy with what you’ve achieved!",
+  "Everyday is a day to be happy!",
+];
+
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "positive-vibe" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "positive-vibe" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "positive-vibe.helloWorld",
+    () => {
+      cron.schedule("* * * * *", () => {
+        vscode.window.showInformationMessage(
+          postivesVibeData[Math.floor(Math.random() * postivesVibeData.length)]
+        );
+      });
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('positive-vibe.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		cron.schedule('* * * * *', () => {
-			vscode.window.showInformationMessage('Hello World from positive vibe!');
-		});
-
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
